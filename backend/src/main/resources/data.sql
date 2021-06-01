076a062219c7a91559551577614f875391044c0b
Leonel De Sousa <leonel55sousa@gmail.com>
2021-06-01 20:46:55 -0300

    DTO

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO tb_category (name) VALUES ('Livros');
-INSERT INTO tb_category (name) VALUES ('Livros');
-INSERT INTO tb_category (name) VALUES ('Livros');
+INSERT INTO tb_category (name) VALUES ('ALETRONICOS');
+INSERT INTO tb_category (name) VALUES ('PORTABLE');
