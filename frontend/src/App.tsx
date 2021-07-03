bd76050b9b547b8fce971815625c464b7403bbac
Leonel De Sousa <leonel55sousa@gmail.com>
2021-07-03 19:17:53 -0300

    Delete file exemple props

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,7 @@
-import { useState, useEffect } from "react";
-
-const App = () => {
-
-    const [contador, setContador] = useState(0);
-
-    useEffect(() => {
-        
-        console.log('Iniciando componente')
-    }, [contador]);
-
+const App = () => {   
     return (
-        <div className="container mt-5">
-            <button className="btn btn-primary me-5" onClick={() => setContador(contador + 1)}>
-                +
-            </button>
-            <span>
-                {contador}
-            </span>
-            <button className="btn btn-primary ms-5" onClick={() => setContador(contador - 1)}>
-                -
-            </button>
-            {contador > 5 && <h1>O valor é maior que 5</h1>}
-            {contador <= 5 && <h1>O valor é menor ou igual a 5</h1>}
-        </div>
-    )
+        <h1>App</h1>
+    );
 }
 
 export default App;
