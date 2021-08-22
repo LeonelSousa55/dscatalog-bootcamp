c693bd94dd91749178205e58452df8b19a66e594
Leonel De Sousa <leonel55sousa@gmail.com>
2021-08-22 13:30:35 -0300

    Form product 70%

--- frontend/src/pages/Admin/components/Products/Form/index.tsx
+++ frontend/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,15 +1,70 @@
+import React, { useState } from 'react';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
 
+type FormState = {
+    name: string;
+    price: string;
+    category: string;
+}
+
 const Form = () => {
+    const [formData, setFormData] = useState<FormState>({
+        name: '',
+        price: '',
+        category: ''
+    });
+
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        //...data -> pega o valor que já existe nesse componente 
+        //e acrescenta mais o value, tudo feito de forma dinâmica
+        setFormData(data => ({ ...data, [name]: value }));
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        console.log(formData);
+    }
+
     return (
-        <BaseForm title="CADASTRAR UM PRODUTO">
-            <div className="row">
-                <div className="col-6">
-                    <input type="text" className="form-control" />
+        <form onSubmit={handleSubmit}>
+            <BaseForm title="Cadastrar um produto">
+                <div className="row">
+                    <div className="col-6">
+                        <input
+                            type="text"
+                            className="form-control mb-3"
+                            onChange={handleOnChange}
+                            name="name"
+                            value={formData.name}
+                            placeholder="Nome do produto"
+                        />
+                        <select
+                            className="form-control mb-3"
+                            onChange={handleOnChange}
+                            name="category"
+                            value={formData.category}
+                        >
+                            <option value="livros">Livros</option>
+                            <option value="computadores">Computadores</option>
+                            <option value="eletronicos">Eletrônicos</option>
+                        </select>
+                        <input
+                            type="text"
+                            onChange={handleOnChange}
+                            className="form-control"
+                            name="price"
+                            value={formData.price}
+                            placeholder="Preço"
+                        />
+                    </div>
                 </div>
-            </div>
-        </BaseForm>
+            </BaseForm>
+        </form>
     )
 }
 
