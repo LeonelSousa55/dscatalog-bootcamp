51acdc3aa659eb41b54000d98b107d89ce62e67a
Leonel De Sousa <leonel55sousa@gmail.com>
2021-08-22 17:14:29 -0300

    Insert product 100%, bootcamp 2.0 cap.3.

--- frontend/src/pages/Admin/components/Products/Form/index.tsx
+++ frontend/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,3 +1,4 @@
+import { makeRequest } from 'core/utils/request';
 import React, { useState } from 'react';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
@@ -6,16 +7,20 @@ type FormState = {
     name: string;
     price: string;
     category: string;
+    description: string;
 }
+
+type FormEvent = React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>;
 
 const Form = () => {
     const [formData, setFormData] = useState<FormState>({
         name: '',
-        price: '',
-        category: ''
+        price: '0',
+        category: '1',
+        description: ''
     });
 
-    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const handleOnChange = (event: FormEvent) => {
         const name = event.target.name;
         const value = event.target.value;
 
@@ -26,8 +31,18 @@ const Form = () => {
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        const payload = {
+            //Pegando tudo que já existe no payload e adicionando o resto dos dados
+            ...formData,
+            imgUrl: 'https://images.kabum.com.br/produtos/fotos/sync_mirakl/173977/Console-Playstation-5-Ps5-Sony-Branco-Disc-Version_1625063236_g.jpg',
+            categories: [{ id: formData.category }]
+        }
 
-        console.log(formData);
+        makeRequest({ url: '/products', method: 'POST', data: payload })
+            //Inicializando os valores dos campos após a insersão dos dados
+            .then(() => {
+                setFormData({ name: '', category: '1', price: '0', description: '' });
+            });
     }
 
     return (
@@ -49,9 +64,9 @@ const Form = () => {
                             name="category"
                             value={formData.category}
                         >
-                            <option value="livros">Livros</option>
-                            <option value="computadores">Computadores</option>
-                            <option value="eletronicos">Eletrônicos</option>
+                            <option value="1">Livros</option>
+                            <option value="2">Eletrônicos</option>
+                            <option value="3">Computadores</option>
                         </select>
                         <input
                             type="text"
@@ -60,6 +75,17 @@ const Form = () => {
                             name="price"
                             value={formData.price}
                             placeholder="Preço"
+                        />
+                    </div>
+                    <div className="col-6">
+                        <textarea
+                            className="form-control"
+                            onChange={handleOnChange}
+                            name="description"
+                            value={formData.description}
+                            rows={10}
+                            cols={10}
+                            placeholder="Descrição"
                         />
                     </div>
                 </div>
